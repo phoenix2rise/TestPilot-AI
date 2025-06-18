6466def99d49f60f569b52e92a466abeefe50992
phoenix2rise <dayvid.d1@gmail.com>
2025-06-18 21:17:11 +0200

    Create test_performance_metrics.py

---
+++ tests/performance/test_performance_metrics.py
@@ -0,0 +1,10 @@
+def test_performance_metrics(page):
+    page.goto("https://the-internet.herokuapp.com/login")
+    performance = page.evaluate("""() => {
+        return {
+            domContentLoaded: performance.timing.domContentLoadedEventEnd - performance.timing.navigationStart,
+            load: performance.timing.loadEventEnd - performance.timing.navigationStart
+        };
+    }""")
+    print("Performance metrics:", performance)
+    assert performance['load'] < 3000, "Page load time is too high"
