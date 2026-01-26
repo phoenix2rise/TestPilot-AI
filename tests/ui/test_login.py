fc693d70cfb95d22eb33ad8f761a169e53f5b82c
phoenix2rise <dayvid.d1@gmail.com>
2026-01-26 21:07:00 +0100

    Add files via upload

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,35 +1,32 @@
-from playwright.sync_api import Page, TimeoutError
+import os
+import pytest
+import allure
 
-class LoginPage:
-    def __init__(self, page: Page):
-        self.page = page
-        self.url = "https://the-internet.herokuapp.com/login"
-        self.username_locator = "#username"
-        self.password_locator = "#password"
-        self.login_button = 'button[type="submit"]'
-        self.flash_message = ".flash"
+from pages.login_page import LoginPage
 
-    def load(self):
-        self.page.goto(self.url)
 
-    def login(self, username, password):
-        try:
-            self.page.fill(self.username_locator, username)
-        except:
-            self.page.fill("input[name='username']", username)
-        self.page.fill(self.password_locator, password)
-        self.page.click(self.login_button)
+@allure.feature("Auth")
+@allure.story("Login")
+def test_login_success(page):
+    login_page = LoginPage(page)
 
-    def is_success(self):
-        try:
-            msg = self.page.locator(self.flash_message).inner_text()
-            return "You logged into a secure area!" in msg
-        except TimeoutError:
-            return False
+    # For secure self-heal workflow: force primary locator to break so fallback triggers & is recorded
+    if os.getenv("BREAK_LOCATOR", "false").lower() == "true":
+        login_page.username_locator = "#username_broken"
 
-    def is_error(self):
-        try:
-            msg = self.page.locator(self.flash_message).inner_text()
-            return "Your username is invalid!" in msg or "Your password is invalid!" in msg
-        except TimeoutError:
-            return False
+    login_page.load()
+    login_page.login("tomsmith", "SuperSecretPassword!")
+    assert login_page.is_success(), "Expected successful login message"
+
+
+@allure.feature("Auth")
+@allure.story("Login")
+def test_login_invalid_password(page):
+    login_page = LoginPage(page)
+
+    if os.getenv("BREAK_LOCATOR", "false").lower() == "true":
+        login_page.username_locator = "#username_broken"
+
+    login_page.load()
+    login_page.login("tomsmith", "wrongpassword")
+    assert login_page.is_error(), "Expected invalid credentials message"
